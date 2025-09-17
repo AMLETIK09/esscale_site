2f879738f7a9d846b8092dcea7e5b12182c4ee82
Daria Abrosina <d2881312@gmail.com>
2025-09-17 14:24:11 +0300

    Создание блоков about-us, interests

--- sections/interests/interests.js
+++ sections/interests/interests.js
@@ -1,2 +1,17 @@
 // sections/interests/interests.js
 // JavaScript функциональность для секции "Интересы"
+/* Функция открытия окна */
+const closeButtons = document.querySelectorAll('.interests_close-button');
+  closeButtons.forEach(button => {
+    button.addEventListener('click', (event) => {
+      event.preventDefault();
+      const dialog = button.closest('dialog');
+      if (!dialog) return;
+
+      dialog.classList.add('closing');
+      setTimeout(() => {
+        dialog.close();
+        dialog.classList.remove('closing');
+      }, 300);
+    });
+  });
